7d054b30fe6fbeb0edb689b92d7e7b39e2a43eb2
William Manley <will@williammanley.net>
2017-03-07 12:57:26 +0000

    Tests: Refactor test-no-initramfs
    I'm going to be adding a new test-case, so I don't want to copy-paste all
    this tree creation code again.

--- tests/test-no-initramfs.sh
+++ tests/test-no-initramfs.sh
@@ -17,14 +17,22 @@ assert_not_file_has_content sysroot/boot/loader/entries/ostree-testos-0.conf 'in
 
 echo "ok deployment with initramfs"
 
-cd ${test_tmpdir}/osdata/boot
-rm -f initramfs* vmlinuz*
-echo "the kernel only" > vmlinuz-3.6.0
-bootcsum=$(cat vmlinuz-3.6.0 | sha256sum | cut -f 1 -d ' ')
-mv vmlinuz-3.6.0 vmlinuz-3.6.0-${bootcsum}
-cd -
-${CMD_PREFIX} ostree --repo=${test_tmpdir}/testos-repo commit --tree=dir=osdata/ -b testos/buildmaster/x86_64-runtime
-${CMD_PREFIX} ostree pull testos:testos/buildmaster/x86_64-runtime
+pull_test_tree() {
+    kernel_contents=$1
+    initramfs_contents=$2
+
+    cd ${test_tmpdir}/osdata/boot
+    rm -f initramfs* vmlinuz* devicetree*
+    bootcsum=$(echo -n "$kernel_contents$initramfs_contents" \
+               | sha256sum | cut -f 1 -d ' ')
+    echo -n "$kernel_contents" > vmlinuz-3.6.0-${bootcsum}
+    [ -n "$initramfs_contents" ] && echo -n "$initramfs_contents" > initramfs-3.6.0-${bootcsum}
+    cd -
+    ${CMD_PREFIX} ostree --repo=${test_tmpdir}/testos-repo commit --tree=dir=osdata/ -b testos/buildmaster/x86_64-runtime
+    ${CMD_PREFIX} ostree pull testos:testos/buildmaster/x86_64-runtime
+}
+
+pull_test_tree "the kernel only"
 ${CMD_PREFIX} ostree admin deploy --os=testos --karg=root=/dev/sda2 --karg=rootwait testos:testos/buildmaster/x86_64-runtime
 assert_file_has_content sysroot/boot/loader/entries/ostree-testos-0.conf 'rootwait'
 assert_file_has_content sysroot/boot/loader/entries/ostree-testos-0.conf 'init='
@@ -32,16 +40,7 @@ assert_not_file_has_content sysroot/boot/loader/entries/ostree-testos-0.conf 'in
 
 echo "ok switching to bootdir with no initramfs"
 
-cd ${test_tmpdir}/osdata/boot
-rm -f initramfs* vmlinuz*
-echo "the kernel" > vmlinuz-3.6.0
-echo "initramfs to assist the kernel" > initramfs-3.6.0
-bootcsum=$(cat vmlinuz-3.6.0 initramfs-3.6.0 | sha256sum | cut -f 1 -d ' ')
-mv vmlinuz-3.6.0 vmlinuz-3.6.0-${bootcsum}
-mv initramfs-3.6.0 initramfs-3.6.0-${bootcsum}
-cd -
-${CMD_PREFIX} ostree --repo=${test_tmpdir}/testos-repo commit --tree=dir=osdata/ -b testos/buildmaster/x86_64-runtime
-${CMD_PREFIX} ostree pull testos:testos/buildmaster/x86_64-runtime
+pull_test_tree "the kernel" "initramfs to assist the kernel"
 ${CMD_PREFIX} ostree admin deploy --os=testos --karg-none --karg=root=LABEL=rootfs testos:testos/buildmaster/x86_64-runtime
 assert_file_has_content sysroot/boot/loader/entries/ostree-testos-0.conf 'initrd'
 assert_file_has_content sysroot/boot/loader/entries/ostree-testos-0.conf 'root=LABEL=rootfs'
